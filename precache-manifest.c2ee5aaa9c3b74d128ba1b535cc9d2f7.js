97a5d9a673ff65f0503524263f0fadb9df04f701
Mikkel Bengtsen <mikkelbengtsen@gmail.com>
2020-05-29 23:56:58 +0200

    Updates

---
+++ precache-manifest.c2ee5aaa9c3b74d128ba1b535cc9d2f7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0461ce16ddaa05067cf30a0e472d8d",
+    "url": "/pomodoify/index.html"
+  },
+  {
+    "revision": "04f2f714df47b0859428",
+    "url": "/pomodoify/static/css/2.55f2319f.chunk.css"
+  },
+  {
+    "revision": "7c17561ac82ee001a3db",
+    "url": "/pomodoify/static/css/main.c48cbd92.chunk.css"
+  },
+  {
+    "revision": "04f2f714df47b0859428",
+    "url": "/pomodoify/static/js/2.4e300f6b.chunk.js"
+  },
+  {
+    "revision": "f828ba3f58479656d16ef621c409174d",
+    "url": "/pomodoify/static/js/2.4e300f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c17561ac82ee001a3db",
+    "url": "/pomodoify/static/js/main.48970b4c.chunk.js"
+  },
+  {
+    "revision": "ad716ae5d3ee21cc375b",
+    "url": "/pomodoify/static/js/runtime-main.05b210da.js"
+  },
+  {
+    "revision": "12f3cc362ea6ce6497aacf4b154ebb87",
+    "url": "/pomodoify/static/media/alarm.12f3cc36.mp3"
+  },
+  {
+    "revision": "c3c16267779b3b045fa98d45903fe7f3",
+    "url": "/pomodoify/static/media/cuckoo.c3c16267.mp3"
+  },
+  {
+    "revision": "126a5b6d73a777b5c3e75d070e76339e",
+    "url": "/pomodoify/static/media/logo.126a5b6d.svg"
+  },
+  {
+    "revision": "b685cef5416e5914b679a70c40a0f0b7",
+    "url": "/pomodoify/static/media/tick.b685cef5.mp3"
+  },
+  {
+    "revision": "67c7415a7471165ed0355b43d549a9bc",
+    "url": "/pomodoify/static/media/yoo.67c7415a.mp3"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pomodoify/precache-manifest.c2ee5aaa9c3b74d128ba1b535cc9d2f7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomodoify/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
